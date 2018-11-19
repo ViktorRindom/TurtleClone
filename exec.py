d92d26771dedc3eadc474a57f622748c7d9b1e8a
ViktorRindom <43568804+ViktorRindom@users.noreply.github.com>
2018-11-19 10:48:37 +0100

    exec

--- exec.py
+++ exec.py
@@ -1,2 +1,8 @@
-exec(input("run code: "))
-input("done")
+t = True
+while t:
+	try:
+		exec(input("run code: "))
+		t=False
+	except (SyntaxError, NameError, AttributeError, ValueError):
+		print("error Try again")
+print("well done")
