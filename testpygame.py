d12c0251171ca0c8be5d537ad0983234efd9f29d
ViktorRindom <43568804+ViktorRindom@users.noreply.github.com>
2018-11-27 11:08:49 +0100

    asdaasdf

--- testpygame.py
+++ testpygame.py
@@ -1,18 +1,2 @@
-import pygame
-
-(width, height) = (300, 200)
-color=(10,10,10)
-pos=(10,10)
-screen = pygame.display.set_mode((width, height))
-tf=True
-while tf:	
-	pygame.event.get()
-	keys = pygame.key.get_pressed()
-	if pygame.event.EventType == pygame.QUIT:
-		tf = False
-	pygame.draw.circle(screen,color,pos,5,0)
-	pygame.display.flip()
-	pygame.time.delay(500)
-
-
-pygame.quit
+a = [x = 0,x = 1]
+print(a[1])
